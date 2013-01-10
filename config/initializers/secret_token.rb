44efd318d834a788a00c163e6102f22500bf56e9
James Smith <st23am@gmail.com>
2013-01-10 12:16:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HydrasVsEachOther::Application.config.secret_token = '0d8be23fbde3d4111af95af922a3eba868610452a935e1fe633f92792165b186c1cdbb54e2e3475c4b8a4fecffd19198646eac2571c0c08ff2870ad12af0641e'
